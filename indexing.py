ae37e17ce7d7eb809a944703f375e2bb0a51016f
Deepshikha <109437680+Deepshikha0105@users.noreply.github.com>
2022-07-27 05:31:18 +0530

    Create indexing.py

---
+++ indexing.py
@@ -0,0 +1,9 @@
+'''ques1:
+data = [22,23,[55,0],0.1,33,44,[12,78,95,26],10,33]
+#1. Parse for 0
+#2. Parse for 0.1
+#2. Parse for 10'''
+
+print("Ans 1.1:", (data[2][1]))
+print("Ans 1.2:", (data[3]))
+print("Ans 1.3:", (data[7])
